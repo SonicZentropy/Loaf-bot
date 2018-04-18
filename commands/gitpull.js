a24adc329a5b37b5f7854d5500e9338e20f416a1
Cory Koseck <funnyman1120@hotmail.com>
2018-04-18 10:12:31 -0500

    Update gitpull.js

--- commands/gitpull.js
+++ commands/gitpull.js
@@ -7,15 +7,10 @@ module.exports = {
 		if(message.author.id !== "92838401044140032" && message.author.id !== "107967155928088576")
 			return;
 	    
-	    	await message.channel.send("Updated").then(function(message) { message.react("👍") });
+	    	await message.channel.send("Updated");
 	    	message.delete();
 	    
-	    try
-	    {
         shell.exec('git pull && node .');
 		process.exit();
-	    }
-	    catch(err)
-		    message.channel.send(err);
     },
 };
