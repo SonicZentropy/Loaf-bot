c8ac37426aa820dba13fd44a33665af4124b3fe4
Death <DeathGameDev@gmail.com>
2018-04-18 02:01:28 -0500

    Added async to gitpull

--- commands/gitpull.js
+++ commands/gitpull.js
@@ -3,7 +3,7 @@ const shell = require('shelljs');
 module.exports = {
     name: 'gitpull',
     description: 'Pulls from git!',
-    execute(message, args) {
+    async execute(message, args) {
 		if(message.author.id !== "92838401044140032" && message.author.id !== "107967155928088576")
 			return;
 		await message.delete();
